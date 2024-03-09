3cb7f074d0bbfe9dd4088ad26dbeacc3a989ac47
Artem <ladno0001@gmail.com>
2024-03-09 20:55:47 +0400

    init

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Обо мне</h1>
 <p>Люблю поспать</p>
 
-
+import <video src="/src/sleep.mp4"></video>
 
 <video width="500" height="400" autoplay loop muted>
 <source src="/src/sleep.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
