2a3dd398b01a2f2d8a6cdf844321c51ce55c6bd1
Artem <ladno0001@gmail.com>
2024-03-09 21:37:52 +0400

    init

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,16 +2,21 @@
 <p>Люблю поспать</p>
 
 <script>
-    import photo from './sleep.png';
+    import video1 from './sleep.mp4';
 </script>
 
-<img src={photo} width="300" height="400" alt="Описание изображения"  />
+<video width="500" height="400" autoplay loop muted>
+<source src={video1} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+</video>
+
 <style>
 
 :root {
     --primary-color: #3498db;
 }
-
+    video {
+  margin-left: -100px; 
+}
 h1, h2 {
     color: var(--primary-color);
 }
