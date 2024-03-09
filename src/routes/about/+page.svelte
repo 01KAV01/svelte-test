3cca0340de9601fddd4d981b7e5b77a27a8757cd
Artem <ladno0001@gmail.com>
2024-03-09 21:16:09 +0400

    init

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,19 +1,16 @@
 <h1>Обо мне</h1>
 <p>Люблю поспать</p>
 
-<video src={'/src/sleep.mp4'}/>
-<video width="500" height="400" autoplay loop muted>
-<source src="/src/sleep.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
-</video>
+
+<img src="/src/components/sleep.png" width="300" height="400">
+
 
 <style>
 
 :root {
     --primary-color: #3498db;
 }
-    video {
-  margin-left: -100px; 
-}
+
 h1, h2 {
     color: var(--primary-color);
 }
